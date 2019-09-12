cad7163c26090efad6202f9f60e9f9b92e4f0626
yongzhi yang <yangyongzhi2008@gmail.com>
2019-09-13 00:27:37 +0800

    Update 00.run.sh

--- example/00.run.sh
+++ example/00.run.sh
@@ -1,4 +1,4 @@
-perl ../bin/wgddetector.pl --input_cds test.cds.fa --input_pep test.pep.fa --output_dir out --tmp_dir tmp --thread_num 4 --cluster_engine mmseqs2 --clean no
+perl ../bin/wgddetector.pl --input_cds test.cds.fa --input_pep test.pep.fa --output_dir output --tmp_dir tmp --thread_num 4 --cluster_engine mmseqs2 --clean no
 
 Rscript example.Rscript
 
